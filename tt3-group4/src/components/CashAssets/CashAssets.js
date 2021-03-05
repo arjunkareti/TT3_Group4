0346d213b0892a039c0dc3d450b08b966a1a4085
HaiYong-Lim <80045159+HaiYong-Lim@users.noreply.github.com>
2021-03-05 15:54:54 +0800

    add in styling to CastAssets.js and add in CashAsset.css

--- tt3-group4/src/components/CashAssets/CashAssets.js
+++ tt3-group4/src/components/CashAssets/CashAssets.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import '../../App.css';
 import axios from 'axios';
-  
+import Table from 'react-bootstrap/Table'
+import './CashAssets.css'
+
 const CashAssets = () =>{
 
   const xApiKey = 'mcOtxKkyOR4OwrbGK0Czq8HRmlMcsQ2G1Er2jfy7'
@@ -27,24 +29,31 @@ const CashAssets = () =>{
   },[]);
   
     return (
-      <table>
-        <tr>
-          <th>
-            Asset Balance
-          </th>
-          <th>
-            Cash Balance
-          </th>
-        </tr>
-        <tr>
-          <th>
-            {state.balances.assetBalance}
-          </th>
-          <th>
-          {state.balances.cashBalance}
-          </th>
-        </tr>
-      </table>
+      <div className="divContainer">
+        <h1>Total Assets</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>
+                Asset Balance
+              </th>
+              <th>
+                Cash Balance
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>
+                {state.balances.assetBalance}
+              </th>
+              <th>
+              {state.balances.cashBalance}
+              </th>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
       );
 }
 export default CashAssets;
