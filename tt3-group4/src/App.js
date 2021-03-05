3ae8b837056fbd5c0c27c1b096ab583981291267
akareti@me.com <akareti@me.com>
2021-03-05 12:20:40 +0800

    Login page and Nav

--- tt3-group4/src/App.js
+++ tt3-group4/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Switch ,Route} from 'react-router-dom';
 import './App.css';
 import TransactionHistory from './components/transactionHistory/TransactionHistory'
+import LoginPage from './components/LoginPage';
+import Nav from './components/nav.components';
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <TransactionHistory />
+
+    <Nav/>
+    <Switch>
+    <Route exact path="/login" component={LoginPage}/>
+    </Switch>
     </div>
+    </BrowserRouter>
   );
 }
 
