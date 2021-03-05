fc858b1819d703f45d6f4aa301d510c92cbb9b83
HaiYong-Lim <80045159+HaiYong-Lim@users.noreply.github.com>
2021-03-05 14:32:38 +0800

    Add in CashAssets.js

--- tt3-group4/src/App.js
+++ tt3-group4/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TransactionHistory from './components/transactionHistory/TransactionHistory'
+import CashAssets from './components/CashAssets/CashAssets'
 
 function App() {
   return (
     <div className="App">
       <TransactionHistory />
+      <CashAssets />
     </div>
   );
 }
---
+++ tt3-group4/src/components/CashAssets/CashAssets.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+  
+const CashAssets = () =>{
+
+  const xApiKey = 'mcOtxKkyOR4OwrbGK0Czq8HRmlMcsQ2G1Er2jfy7'
+  const accountKey = '58cf2a3d-4e73-48c6-a7d1-437e8d70e181'
+  const axioConfig = {
+    headers:{
+      'x-api-key' : xApiKey
+    }   
+  }
+  const body = {
+
+    'accountKey' : accountKey
+       
+  }
+  const [state,setState] = useState({
+    balances: [],
+  });
+
+  useEffect(()=>{
+    axios.get('https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/balance',body,axioConfig).then(res => {
+        console.log(res);
+        setState({...state,balances: res.data});
+    });
+  },[]);
+  
+    return (
+      <table>
+        <tr>
+          <th>
+            Asset Balance
+          </th>
+          <th>
+            Cash Balance
+          </th>
+        </tr>
+        <tr>
+          <th>
+            {state.balances.assetBalance}
+          </th>
+          <th>
+          {state.balances.cashBalance}
+          </th>
+        </tr>
+      </table>
+      );
+}
+export default CashAssets;
