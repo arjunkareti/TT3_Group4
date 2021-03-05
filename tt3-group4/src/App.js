3670ae8de2a972c59ba57bad43296e5a7ece62bf
HaiYong-Lim <80045159+HaiYong-Lim@users.noreply.github.com>
2021-03-05 14:45:09 +0800

    add navbar

--- tt3-group4/src/App.js
+++ tt3-group4/src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TransactionHistory from './components/transactionHistory/TransactionHistory'
 import CashAssets from './components/CashAssets/CashAssets'
+import {BrowserRouter as Router, NavLink,Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <TransactionHistory />
-      <CashAssets />
-    </div>
+    <Router>
+      <div className="App">
+        <NavLink activeClassName="active" to = "/Components/CashAssets/CashAssets">Cash/Asset Balance</NavLink>
+        <NavLink activeClassName="active" to = "/Components/TransactionHistory/TransactionHistory">Transaction History</NavLink>
+        <hr></hr>
+        <Switch>
+          <Route  exact path="/Components/CashAssets/CashAssets" component={CashAssets}></Route>
+          <Route  exact path="/Components/TransactionHistory/TransactionHistory" component={TransactionHistory}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
